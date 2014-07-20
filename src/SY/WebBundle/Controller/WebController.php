72bc3d357586df77560094018e3b86bd59d194f2
Quique <etorras@gmail.com>
2014-07-20 22:02:03 +0200

    LLamada a la web y envio por json

--- src/SY/WebBundle/Controller/WebController.php
+++ src/SY/WebBundle/Controller/WebController.php
@@ -3,11 +3,39 @@
 namespace SY\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class WebController extends Controller
 {
     public function indexAction()
     {
         return $this->render('SYWebBundle:Web:index.html.twig');
+    }
+
+    public function loadWebAjaxAction()
+    {
+        $request = $this->get('request');
+        $url=$request->request->get('url');
+
+        if('' != $url){
+            $html = $this->getHTML($url);
+            $return=array("responseCode" => 200, "html" => $html);
+        }
+        else{
+            $return=["responseCode" => 400, "html" => "Error"];
+        }
+
+        return new Response(json_encode($return), 200, ['Content-Type'=>'application/json']);
+    }
+
+    private function getHTML($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
     }
 }
